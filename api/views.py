c1112676b5360f5cba8cebcaed0d24ae6247f12d
Will Weatherford <weatherford.william@gmail.com>
2016-08-27 15:50:06 -0700

    added single Drug item detail view.

--- api/views.py
+++ api/views.py
@@ -1,10 +1,17 @@
 """Establish views for API access to Drug database."""
 from drugs.models import Drug
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from api.serializers import DrugSerializer
 
 
 class DrugListView(ListAPIView):
+    """View allowing API access to view lists of owner's photos."""
+
+    queryset = Drug.objects.all()
+    serializer_class = DrugSerializer
+
+
+class DrugDetailView(RetrieveAPIView):
     """View allowing API access to view lists of owner's photos."""
 
     queryset = Drug.objects.all()
