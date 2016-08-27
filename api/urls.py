4349b5bbae3b08b7e5d0ee32e20b5611abfba4da
Will Weatherford <weatherford.william@gmail.com>
2016-08-27 16:01:11 -0700

    fixed detail url pattern.

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ from api.views import DrugListView, DrugDetailView
 
 urlpatterns = [
     url(r'drugs/list$', DrugListView.as_view(), name='drug_list'),
-    url(r'drugs/[0-9]+$', DrugDetailView.as_view(), name='drug_detail'),
+    url(r'drugs/(?P<pk>[0-9]+)$', DrugDetailView.as_view(), name='drug_detail'),
 ]
