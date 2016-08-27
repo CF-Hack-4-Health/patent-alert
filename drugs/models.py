d625bf0092dff04dd7d44f14907ac9c1a6473cef
Will Weatherford <weatherford.william@gmail.com>
2016-08-27 13:26:27 -0700

    fixed typo Charfield

--- drugs/models.py
+++ drugs/models.py
@@ -21,15 +21,15 @@ patent_expiration_date
 class Drug(md.Model):
     """Database model of a pharmacuetical product."""
 
-    ingredient = md.Charfield(max_length=255)
-    dosage_type = md.Charfield(max_length=255)
-    delivery_method = md.Charfield(max_length=255)
-    trade_name = md.Charfield(max_length=255)
-    owner_name = md.Charfield(max_length=255)
-    drug_strength = md.Charfield(max_length=255)
-    drug_application_number = md.Charfield(max_length=255)
-    product_number = md.Charfield(max_length=255)
-    patent_number = md.Charfield(max_length=255)
+    ingredient = md.CharField(max_length=255)
+    dosage_type = md.CharField(max_length=255)
+    delivery_method = md.CharField(max_length=255)
+    trade_name = md.CharField(max_length=255)
+    owner_name = md.CharField(max_length=255)
+    drug_strength = md.CharField(max_length=255)
+    drug_application_number = md.CharField(max_length=255)
+    product_number = md.CharField(max_length=255)
+    patent_number = md.CharField(max_length=255)
     approval_date = md.DateField(max_length=255)
     patent_expiration_date = md.DateField(max_length=255)
 
