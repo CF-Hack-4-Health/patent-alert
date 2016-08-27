333d33e1901d3d126f248075eeceb9401ecb3ac6
Will Weatherford <weatherford.william@gmail.com>
2016-08-27 14:02:55 -0700

    set up Drug.__init__() to convert dates to correct format.

--- drugs/models.py
+++ drugs/models.py
@@ -2,6 +2,9 @@
 from __future__ import unicode_literals
 from django.db import models as md
 
+from datetime import datetime
+INPUT_DATE_FORMAT = '%b %d, %Y'
+OUTPUT_DATE_FORMAT = '%Y-%m-%d'
 
 """
 ingredient,
@@ -32,6 +35,20 @@ class Drug(md.Model):
     patent_number = md.CharField(max_length=255)
     approval_date = md.DateField(max_length=255)
     patent_expiration_date = md.DateField(max_length=255)
+
+    def __init__(self, *args, approval_date='', patent_expiration_date='', **kwargs):
+        if approval_date:
+            approval_date = datetime.strptime(approval_date, INPUT_DATE_FORMAT)
+            approval_date = approval_date.strftime(OUTPUT_DATE_FORMAT)
+        if patent_expiration_date:
+            patent_expiration_date = datetime.strptime(patent_expiration_date, INPUT_DATE_FORMAT)
+            patent_expiration_date = patent_expiration_date.strftime(OUTPUT_DATE_FORMAT)
+        super(Drug, self).__init__(
+            *args,
+            approval_date=approval_date,
+            patent_expiration_date=patent_expiration_date,
+            **kwargs,
+        )
 
     def __str__(self):
         return self.trade_name
