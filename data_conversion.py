f1c264328c2d9b4cd0703f7598ccfd02dc13ed4b
muniri92 <mibrah04@gmail.com>
2016-08-27 16:34:40 -0700

    added a populated DB

--- data_conversion.py
+++ data_conversion.py
@@ -5,10 +5,9 @@ from drugs.models import Drug
 
 def conversion():
     """Convert from JSON into database."""
-    with open('sample_dict.txt', 'r') as sample_file:
+    with open('../sample_dict.txt', 'r') as sample_file:
         lst = eval(sample_file.read())
         for data in lst:
-            print(data)
             dmodel = Drug(**data)
             dmodel.save()
 
