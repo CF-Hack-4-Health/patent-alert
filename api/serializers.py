a17333edf7063999ab098f82c81f10773acf77d8
Will Weatherford <weatherford.william@gmail.com>
2016-08-27 18:25:17 -0700

    Initial attempt to add a date_delta field.

--- api/serializers.py
+++ api/serializers.py
@@ -2,10 +2,19 @@
 from __future__ import unicode_literals
 from rest_framework import serializers
 from drugs.models import Drug
+import datetime
 
 
 class DrugSerializer(serializers.HyperlinkedModelSerializer):
     """Serializer for the Drug model."""
+
+    date_delta = serializers.SerializerMethodField('_calc_date_delta')
+
+    def _calc_date_delta(self):
+        """Return an integer of days until the expiration of the patent."""
+        now = datetime.datetime.now()
+        delta = datetime.timedelta(self.patent_expiration_date, now)
+        return delta.days
 
     class Meta:
         """Meta for DrugSerializer."""
